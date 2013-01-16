96da235a93ffddeb731b349047ccaa9869c67f52
jhahkala <joni.hahkala@cern.ch>
2013-01-16 02:32:57 +0100

    format and comment the test a bit

--- src/test/java/org/glite/security/delegation/DelegationTest.java
+++ src/test/java/org/glite/security/delegation/DelegationTest.java
@@ -19,15 +19,16 @@ public class DelegationTest extends TestCase {
 
     public void testDelegation() throws Exception {
 
-      Logger LOGGERRoot = Logger.getLogger("org.glite.security");
-      Layout lay = new PatternLayout("%d{ISO8601} %-5p [%t] %l %x - %m%n");
-      Appender appender = new ConsoleAppender(lay);
-      LOGGERRoot.addAppender(appender);
-      LOGGERRoot.setLevel(Level.DEBUG);
-        
+        Logger LOGGERRoot = Logger.getLogger("org.glite.security");
+        Layout lay = new PatternLayout("%d{ISO8601} %-5p [%t] %l %x - %m%n");
+        Appender appender = new ConsoleAppender(lay);
+        LOGGERRoot.addAppender(appender);
+        LOGGERRoot.setLevel(Level.DEBUG);
+
         String proxyfile = "src/test/certs/trusted_client.proxy.grid_proxy";
         String delegationId = "testID";
-        
+
+        // server side request generation
         GrDProxyDlgeeOptions opts = new GrDProxyDlgeeOptions();
         opts.setDlgeeKeySize(2048);
         opts.setDlgeeStorage("target");
@@ -40,16 +41,19 @@ public class DelegationTest extends TestCase {
 
         String req = delegation.getProxyReq(delegationId, certChain);
         System.out.println(req);
+
         
+        // client side new proxy signing
         GrDProxyDlgorOptions dopts = new GrDProxyDlgorOptions();
         dopts.setDlgorCertFile(proxyfile);
-        
+
         DelegationHandler handler = new DelegationHandler(req, delegationId, dopts);
         String certString = handler.getPEMProxyCertificate();
         System.out.println(certString);
-        
+
+        // server side new proxy storage
         delegation.putProxy(delegationId, certString, certChain);
-        
+
     }
 
 }
